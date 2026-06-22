2c4e9fd5689696aee7cc9b615a1ecd2374ac57ee
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2026-06-22 19:14:08 +0530

    feat: add TURN server fallback for symmetric NAT traversal

--- peerspace/js/config.js
+++ peerspace/js/config.js
@@ -8,6 +8,29 @@ const PS = Object.freeze({
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
+      {
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80",
+        username: "e5b32916d49eafeb4be017c0",
+        credential: "8F08omBCRLzJUWEP",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username: "e5b32916d49eafeb4be017c0",
+        credential: "8F08omBCRLzJUWEP",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:443",
+        username: "e5b32916d49eafeb4be017c0",
+        credential: "8F08omBCRLzJUWEP",
+      },
+      {
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username: "e5b32916d49eafeb4be017c0",
+        credential: "8F08omBCRLzJUWEP",
+      }
     ]
   },
   // DataChannel labels
